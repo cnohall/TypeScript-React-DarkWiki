66f514de8b7702618f0c1831c78a171960c9c83d
Christopher <christopher.nohall@gmail.com>
2019-10-12 13:01:54 +0800

    basic setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,56 @@
 import React from 'react';
-import logo from './logo.svg';
+import { render } from 'react-dom';
 import './App.css';
+import { string } from 'prop-types';
 
 
 let page;
 const proxy = "https://cors-anywhere.herokuapp.com/";
 const api = `${proxy}http://en.wikipedia.org/w/api.php?action=parse&section=0&prop=text&page=${page}`
+// class App extends React.Component<{}, IState> {
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+
+class App extends React.Component <{}, { value: string }> {
+  constructor(props:any) {
+    super(props);
+    
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event:any) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event:any) {
+    alert('A name was submitted: ' + this.state.value); //error here
+    event.preventDefault();
+  }
+
+
+  
+  public render(){
+    return (
+      <div className="App">
+        <header className="App-header">
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          DarkWiki by cnohall
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <form onSubmit={this.handleSubmit}>
+        <label>
+          WikiPage:
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+        </form>
+        </header>
+      </div>
+
+    );
+  }
 }
 
 export default App;
